ec1aa8f4533b4b1f4242a3ff68a58dda8df1f3ff
Tom Regner <tom@goochesa.de>
2013-06-03 12:47:16 +0200

    added copyright/license info

--- php/elisp-info.php
+++ php/elisp-info.php
@@ -1,5 +1,29 @@
 <?php
+/* ;; Copyright (C) 2013 Tom Regner
 
+;;
+;; Author: Tom Regner
+;; Maintainer: Tom Regner <tom@goochesa.de>
+;;
+;;
+;;  This file is NOT part of GNU Emacs
+
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;  This file provides php functions that emacs can call in a boris comint process
+*/
 if(!defined("ELISP_INFO_LOADED")) {
     define("ELISP_INFO_LOADED", TRUE);
     define("RESULT_MARK_BEGIN", ";; -- php completion begin ;;");
